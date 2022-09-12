de64a86590b8f67d3ff08ff26d145253b9472171
yash-shah17 <88420125+yash-shah17@users.noreply.github.com>
2022-09-12 14:03:12 +0530

    Add files via upload

---
+++ src/main/java/in/ac/charusat/studentmgmtsystem/controller/StudentController.java
@@ -0,0 +1,85 @@
+package in.ac.charusat.studentmgmtsystem.controller;
+
+import in.ac.charusat.studentmgmtsystem.model.Student;
+import in.ac.charusat.studentmgmtsystem.payload.LoginRequest;
+import in.ac.charusat.studentmgmtsystem.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@CrossOrigin(origins="http://localhost:3000")
+public class StudentController {
+
+    @Autowired
+    StudentRepository studentRepository;
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+//    List<Student> students = new ArrayList<>(
+//            Arrays.asList(
+//                    new Student(1, "Tom", "US"),
+//                    new Student(2, "Harry", "Canada"),
+//                    new Student(3, "Nick", "UK")
+//            )
+//    );
+
+    // Mappings - URL endpoints
+    // Get the list of all student
+    @PostMapping("/login")
+    public String doLogin(@RequestBody LoginRequest loginRequest){
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                loginRequest.getUsername(),loginRequest.getPassword()
+        ));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return "Log in Success!!!";
+    }
+
+    @GetMapping
+    public String displayWelcomeMessage(){
+        return "<center><h1>Welcome to the Spring Boot Security!!!!</h1></center>";
+    }
+    @GetMapping("/listStudents")
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    } // Select * from student;
+
+    // Get the student information
+    @GetMapping("/student/{id}")
+    public Student getStudent(@PathVariable Integer id) {
+        return studentRepository.findById(id).get();
+    } // Select * from student where id=?
+
+    // Delete the student
+    @DeleteMapping("/student/{id}") // delete from student where id=?
+    public List<Student> deleteStudent(@PathVariable Integer id) {
+        studentRepository.delete(studentRepository.findById(id).get());
+        return studentRepository.findAll();
+    }
+
+    // Add new student
+    @PostMapping("/student") // insert into student values(?, ?, ?)
+    public List<Student> addStudent(@RequestBody Student student) {
+        studentRepository.save(student);
+        return studentRepository.findAll();
+    }
+
+    // Update the student information
+    @PutMapping("/student/{id}") // update table student set name=? where id=?
+    public List<Student> updateStudent(@RequestBody Student student, @PathVariable Integer id) {
+        Student studentObj = studentRepository.findById(id).get();
+        studentObj.setName(student.getName());
+        studentObj.setAddress(student.getAddress());
+        studentRepository.save(studentObj);
+        return studentRepository.findAll();
+    }
+
+}
